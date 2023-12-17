763bb3251b14814d2ba33a8618fdcd5643be89fa
SerdarMenekli <116300043+SerdarMenekli@users.noreply.github.com>
2023-12-17 13:15:51 +0300

    added account management

--- app/auth/models.py
+++ app/auth/models.py
@@ -9,19 +9,24 @@ class User(db.Model):
     __tablename__ = 'user'
     
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100))
+    username = db.Column(db.String(100), unique=True, nullable=False)
     pwdhash = db.Column(db.String())
+    email = db.Column(db.String(100), unique=True, nullable=False)
     admin = db.Column(db.Boolean())
     
     customer = db.relationship('Customer', backref='user', uselist=False)
     
-    def __init__(self, username, password, admin=False):
+    def __init__(self, username, password, email, admin=False):
         self.username = username
         self.pwdhash = generate_password_hash(password)
+        self.email = email
         self.admin = admin
         
     def check_password(self, password):
         return check_password_hash(self.pwdhash, password)
+    
+    def set_password(self, password):
+        self.pwdhash = generate_password_hash(password)
     
     def is_admin(self):
         return self.admin
@@ -54,4 +59,15 @@ class LoginForm(FlaskForm):
     username = StringField('Username', [DataRequired()])
     password = PasswordField('Password', [DataRequired()])
     
-    
+class ManageAccountForm(FlaskForm):
+    password = PasswordField('Password', [DataRequired()])
+    username = StringField('Username')
+    new_password = PasswordField('New Password', [EqualTo('confirm', message='Passwords must match')])
+    confirm = PasswordField('Confirm Password')
+    email = EmailField('Email')
+
+class ManagePersonalForm(FlaskForm):
+    full_name = StringField('Full Name', validators=[DataRequired()])
+    phone_number = StringField('Phone Number')
+    address = StringField('Address')
+    date_of_birth = DateField('Date of Birth', format='%Y-%m-%d', validators=[DataRequired()])
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,6 @@
 from flask import request, render_template, flash, redirect, url_for, session, Blueprint
 from app import db
-from app.auth.models import User, RegistrationForm, LoginForm
+from app.auth.models import User, RegistrationForm, LoginForm, ManageAccountForm, ManagePersonalForm
 from app.rent4u.models import Customer
 from flask import g
 from flask_login import current_user, login_user, logout_user, login_required
@@ -27,13 +27,18 @@ def register():
     if form.validate_on_submit():
         username = request.form.get("username")
         password = request.form.get("password")
-        existing_username = User.query.filter(
-            User.username.like("%" + username + "%")
-        ).first()
+        existing_username = User.query.filter_by(username=form.username.data).first()
+        # existing_username = User.query.filter(User.username.like("%" + username + "%")).first()
         if existing_username:
             flash("This username has been already taken. Try another one.", "warning")
             return render_template("register.html", form=form)
-        user = User(username, password)
+        
+        email=request.form.get("email")
+        existing_email = User.query.filter(User.email.like("%" + email + "%")).first()
+        if existing_email:
+            flash("This email has been already taken. Try another one.", "warning")
+            return render_template("register.html", form=form)
+        user = User(username, password, email)
         db.session.add(user)
         db.session.commit()
         
@@ -82,3 +87,70 @@ def logout():
         
     logout_user()
     return redirect(url_for("rent4u.index"))
+
+
+@auth.route("/manage")
+@login_required
+def manage():
+    form1 = ManageAccountForm()
+    form2 = ManagePersonalForm()
+    user = current_user
+    customer = user.customer
+    return render_template("manage.html", form1=form1, form2=form2, user=user, customer=customer)
+
+@auth.route("/manage_account", methods=["POST"])
+@login_required
+def manage_account():
+    form = ManageAccountForm()
+    if form.validate_on_submit():
+        user = current_user
+        
+        username = request.form.get("username")
+        password = request.form.get("password")
+        
+        if user.check_password(password):
+            if user.username != username:
+                existing_username = User.query.filter_by(username=form.username.data).first()
+                if existing_username:
+                    flash("This username has been already taken. Try another one.", "warning")
+                    return redirect(url_for("auth.manage"))
+                user.username = form.username.data
+            
+            if form.new_password.data:
+                user.set_password(form.new_password.data)
+                pass
+            
+            if user.email != form.email.data:
+                existing_email = User.query.filter_by(email=form.email.data).first()
+                if existing_email:
+                    flash("This email has been already taken. Try another one.", "warning")
+                    return redirect(url_for("auth.manage"))
+                user.email = form.email.data
+        else:
+            flash("incorrect password", "warning")
+            return redirect(url_for("auth.manage"))
+        
+        db.session.commit()
+        flash("Account information updated successfully", "success")
+    if form.errors:
+        flash(form.errors, "danger")
+    return redirect(url_for("auth.manage"))
+
+@auth.route("/manage_personel", methods=["POST"])
+@login_required
+def manage_personal():
+    form = ManagePersonalForm()
+    if form.validate_on_submit():
+        user = current_user
+        customer = user.customer
+        
+        customer.full_name = form.full_name.data
+        customer.phone_number = form.phone_number.data
+        customer.address = form.address.data
+        customer.date_of_birth = form.date_of_birth.data
+        
+        db.session.commit()
+        flash("Account information updated successfully", "success")
+    if form.errors:
+        flash(form.errors, "danger")
+    return redirect(url_for("auth.manage"))
