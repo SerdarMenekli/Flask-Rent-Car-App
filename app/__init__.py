5d5305922dc7b35e73dba650cd358380d5c009e6
SerdarMenekli <116300043+SerdarMenekli@users.noreply.github.com>
2023-12-19 16:27:30 +0300

    added admin panel

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
 from flask_login import LoginManager
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
+
 
 app = Flask(__name__)
 
@@ -22,6 +26,20 @@ app.secret_key = 'some_random_key'
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'auth.login'
+
+
+
+admin = Admin(app, name='Admin Panel', template_mode='bootstrap4')
+
+from app.auth.models import User
+from app.rent4u.models import Customer, Car, Rental, Location, Reservation, Invoice
+admin.add_view(ModelView(User, db.session))
+admin.add_view(ModelView(Customer, db.session))
+admin.add_view(ModelView(Car, db.session))
+admin.add_view(ModelView(Rental, db.session))
+admin.add_view(ModelView(Location, db.session))
+admin.add_view(ModelView(Reservation, db.session))
+admin.add_view(ModelView(Invoice, db.session))
 
 from app.auth.views import auth
 app.register_blueprint(auth)
